fba1b2e9eb7d7d10ef020fc238b780d14f164b8e
Diep Thuy <boychaos9x@gmail.com>
2020-05-05 14:16:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function takeANumber (arr, name){
   arr.push(name);
 console.log(`Welcome,${name}. You are number ${arr.indexOf(name)+1} in line`)
 }
+
+function nowServing (arr)[
+  if( arr.length ==0){
+    return 'There is nobody waiting to be served'
+  }
+  return arr.shift();
+]
