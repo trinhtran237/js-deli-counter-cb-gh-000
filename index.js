d98f8a9d948aa36dabfed434cf07d62b3bbdb2c1
Diep Thuy <boychaos9x@gmail.com>
2020-05-05 14:22:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ console.log(`Welcome,${name}. You are number ${arr.indexOf(name)+1} in line`)
 }
 
 function nowServing (arr){
-  if( arr.length ==0){
-    return 'There is nobody waiting to be served'
-  }
-  return arr.shift();
+  // if( arr.length ==0){
+  //   return 'There is nobody waiting to be served'
+  // }
+  // return arr.shift();
 }
