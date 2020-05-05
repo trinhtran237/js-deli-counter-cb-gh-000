10343b7dea93044886fbec4471ab0d7498ffe259
Diep Thuy <boychaos9x@gmail.com>
2020-05-05 13:55:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function takeANumber (arr, name){
   arr.push(name);
-
-console.log(`Welcome,${name}. You are number ${arr.indexOf(name)} in line`)
+console.log(`Welcome,${name}. You are number ${arr.indexOf(name)+1} in line`)
 }
